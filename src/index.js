a8e260fc3dc48966a290cff2fc73bbd0862decc6
Shvein Anton <t0hashvein@gmail.com>
2019-08-19 19:49:56 +0500

    Some module to test

---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+    /*const IPFS = require('ipfs')
+
+const node = new IPFS()
+
+node.once('ready', () => {
+    return node.repo.stat(data => data);
+});*/
+
+module.exports = {
+    stats: "node.repo.stat",
+}
